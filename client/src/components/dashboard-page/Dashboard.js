bf1c9f2c73eb632144c092634b16bc7d043378a7
bubinq <emilbumov@gmail.com>
2022-08-12 10:54:21 +0300

    Last added changes

--- client/src/components/dashboard-page/Dashboard.js
+++ client/src/components/dashboard-page/Dashboard.js
@@ -1,7 +1,21 @@
+import { useContext } from "react"
+import { GoalContext } from "../../contexts/GoalContext"
 import { Calendar } from "./calendar/Calandar"
 
 
 export const Dashboard = () => {
+    const { goals } = useContext(GoalContext)
+    console.log(goals)
+
+
+    const checkLastGoalValidation = () => {
+        const lastAddedGoal = goals[goals.length - 1]
+        const alreadyExists = goals.find(goal => goal.goal === lastAddedGoal.goal)
+
+        if (alreadyExists) {
+
+        }
+    }
     return (
         <div>
             <Calendar></Calendar>
