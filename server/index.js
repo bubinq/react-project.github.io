bfbd04d0b4b3bc40293a7f0916ea2b4d76d78616
bubinq <emilbumov@gmail.com>
2022-12-05 17:32:37 +0200

    latest changes

--- server/index.js
+++ server/index.js
@@ -34,10 +34,10 @@ app.use("/api/goals", goalRoutes);
 app.use("/api/toDos", toDoRoutes);
 app.use("/api/progress", progressRoutes);
 
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 app.listen(process.env.PORT || 8000, () => {
